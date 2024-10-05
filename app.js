905c92ba37986496f97368dc485fe58c40764ade
Rahul Shrama <sharmarahul2996@gmail.com>
2024-10-05 20:23:59 +0530

    Added gzip compression for performance improvement

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
+const compression = require('compression');
 
 
 const listingRouter = require("./routes/listing.js");
@@ -47,6 +48,7 @@ app.use(express.static(path.join(__dirname, "/public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
+app.use(compression());
 
 
 
